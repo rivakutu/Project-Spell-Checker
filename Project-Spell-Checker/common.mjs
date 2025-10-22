ec558b1dc246b341b466d6be34065f58e1b487ab
rivakutu <revive.mapfumo@students.codeyourfuture.io>
2025-10-22 12:46:10 +0200

    Refactor checkerEngine function for improved readability and structure

--- Project-Spell-Checker/common.mjs
+++ Project-Spell-Checker/common.mjs
@@ -1,3 +1,15 @@
 import words from "./words.json" with { type: "json" };
 
 export const getDictionarySize = () => words.length;
+
+
+// function to check if spelling exists in dictionary
+export function checkerEngine(tokenizedWords){
+    const misspelledWords = [];
+    tokenizedWords.forEach(word => {
+        if (!words.includes(word)) {
+            misspelledWords.push(word);
+        }
+    });
+    return misspelledWords;
+}
