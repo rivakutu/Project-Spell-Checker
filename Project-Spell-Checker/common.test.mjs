5aff6419ebf27cb5daf0aa1bf0a3f49a6e8a32bf
rivakutu <revive.mapfumo@students.codeyourfuture.io>
2025-10-22 12:47:49 +0200

    Add checkerEngine import and corresponding test for misspelled words

--- Project-Spell-Checker/common.test.mjs
+++ Project-Spell-Checker/common.test.mjs
@@ -1,7 +1,16 @@
-import { getDictionarySize } from "./common.mjs";
+import { getDictionarySize,checkerEngine } from "./common.mjs";
 import assert from "node:assert";
 import test from "node:test";
 
 test("Dictionary size is correct", () => {
   assert.equal(getDictionarySize(), 856);
 });
+
+
+//checker engine test
+test("Should identify misspelled words", () => {
+  const tokenizedWords = ["make", "wrld", "this", "a", "tst"];
+  const expectedMisspelled = ["wrld", "tst"];
+  assert.deepEqual(checkerEngine(tokenizedWords), expectedMisspelled);
+});
+
